ffbaa44f85648eda931517d268db3e9af6ee6ad2
ishmail_mgwena <ishmail.mgwena24@gmail.com>
2024-11-21 11:50:12 +0200

    First Phrase Commit

---
+++ src/main/java/com/ishmail_projects_jwt_string_angular/book_network/security/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ishmail_projects_jwt_string_angular.book_network.security;
+
+import com.ishmail_projects_jwt_string_angular.book_network.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
